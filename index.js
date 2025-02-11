2d278d80c15a377c14e7e6a846fd1c4c7db7ae2b
Thomas Robinson <git@tomr.me>
2025-02-11 16:28:29 +0000

    all done?!

--- index.js
+++ index.js
@@ -26,3 +26,34 @@ async function showList(elem, url) {
     elem.append(li);
   }
 }
+
+// 3. Create a function `startShowingMessage` that takes two parameters: an element and a string that is a URL. The function will use `setInterval` to make the following task every 1s: fetch the URL and put the response text into the text content of the provided element.
+function startShowingMessage(elem, url) {
+  window.setInterval(() => {
+    showMessage(elem, url);
+  }, 1000);
+}
+
+// 4. Create a function 'handleError' that accepts an element and a url as a parameter, and shows the fetched text from the server in the element. If there is an error, the function should set the textContent of the element to 'OH DEAR'.
+async function handleError(elem, url) {
+  const rep = await fetch(url);
+  elem.innerHTML = rep.ok ? await rep.text() : 'OH DEAR';
+}
+
+// 5. Create a function `drawBox', which accepts two parameters: a canvas element, and a URL which refers to a simple object with coordinates that you should fetch from a server. The function draws a 10x10 filled black box at the given coordinates. Your drawBox function should update the coordinates and redraw the box every 1 second.
+function drawBox(canvas, url) {
+  const canvasContext = canvas.getContext('2d');
+
+  // function in a function? are we allowing this heresy?
+  async function updateBox() {
+    const rep = await fetch(url);
+    const json = await rep.json();
+
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(json.x, json.y, 10, 10); // top left X, Y; w, h
+  }
+
+  updateBox();
+  window.setInterval(updateBox, 1000);
+}
